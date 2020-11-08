cc1ecd45e66108270c2664d28310fab1981f93d9
Lin LIU <lliu41@slb.com>
2020-11-08 21:54:07 +0800

    Use js reduce method to simplify code

--- main/main.js
+++ main/main.js
@@ -1,18 +1,5 @@
 module.exports = function main(items) {
-
-    // function Item(barcode, name, unit, price) {
-    //     this.barcode = barcode;
-    //     this.name = name;
-    //     this.unit = unit;
-    //     this.price = price;
-    // }
-
-    function UniqueItems(quantity, unit, price) {
-        this.quantity = quantity;
-        this.unit = unit;
-        this.price = price;
-    }
-
+    
     function formatUnit(quantity, unit)
     {
         if(unit !== 'a')
@@ -36,33 +23,35 @@ module.exports = function main(items) {
         var header = '***<store earning no money>Receipt ***\n';
         var hLine = '----------------------\n';
         var end = '**********************\n';
-        var content = [];
-        var total = 0;
-        for (const name of Object.getOwnPropertyNames(uniqueItems)) {
-            let quantity = uniqueItems[name].quantity;
-            let unit = formatUnit(quantity, uniqueItems[name].unit); 
-            let price = uniqueItems[name].price;
+
+        var detail = Object.getOwnPropertyNames(uniqueItems).reduce((content, name) => {
+            let quantity = uniqueItems[name]["count"];
+            let unit = formatUnit(quantity, uniqueItems[name]["item"]["Unit"]);
+            let price = uniqueItems[name]["item"]["Price"];
             let subTotal = quantity * price;
-            total += subTotal;
 
-            content.push('Name: ' + name + ', Quantity: ' + quantity + unit + ', Unit price: ' + price.toFixed(2) + ' (yuan), Subtotal: ' + subTotal.toFixed(2) + ' (yuan)\n');
-        }
+            content.push(`Name: ${name}, Quantity: ${quantity}${unit}, Unit price: ${price.toFixed(2)} (yuan), Subtotal: ${subTotal.toFixed(2)} (yuan)\n`);
+            return content;
+        }, []);
 
-        total = 'Total: ' + total.toFixed(2) + ' (yuan)\n';
+        var totalPrice = Object.getOwnPropertyNames(uniqueItems).reduce((total, name) => {
+            total += uniqueItems[name]["item"]["Price"] * uniqueItems[name]["count"];
+            return total;
+        }, 0);
+        var total = `Total: ${totalPrice.toFixed(2)} (yuan)\n`;
 
-        var receipt = header + content.join('') + hLine + total + end;
-
+        var receipt = header + detail.join('') + hLine + total + end;
         return receipt;
     }
 
-    var uniqueItems = {};
-    for (const item of items) {
-        if (uniqueItems[item.Name] === undefined) {
-            uniqueItems[item.Name] = new UniqueItems(1, item.Unit, item.Price);
-        } else {
-            uniqueItems[item.Name].quantity += 1;
+    const uniqueItems = items.reduce((total, item) => {
+        total[item.Name] = {
+            "count": total[item.Name] ? (total[item.Name]["count"] + 1) : 1,
+            "item": item
         }
-    }
+        return total;
+    }, {});
+
     var result = printReceipt(uniqueItems);
     return result;
 };
